224a5234fea194a5e760651ad9d90afdd1da511c
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-03-26 00:40:40 -0300

    set default header from axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
